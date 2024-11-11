fdca1e6463683686d4f76c2742442281c00b0c17
dakshyashree <dakshyashreejamwal@gmail.com>
2024-11-11 20:05:48 +0530

    new files and folder for project setup added

--- template.py
+++ template.py
@@ -25,9 +25,23 @@ list_of_files = [
     "Dockerfile",
     "requirements.txt",
     "setup.py",
-    "research/trails.ipynb"
+    "research/trails.ipynb",
+    "test.py"
 ]
 
 for filepath in list_of_files:
     filepath = Path(filepath)
-    
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
+
+    if(not os.path.exists(filepath) or (os.path.getsize(filepath)==0)):
+            with open(filepath,'w') as f:
+                 pass
+                 logging.info(f"Creating empty file: {filepath}")
+
+    else:
+         logging.info(f"{filename} is already exists")             
